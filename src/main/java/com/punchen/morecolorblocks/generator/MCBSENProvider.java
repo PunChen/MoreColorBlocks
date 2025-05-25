f5d06b4b424cba207d6ed98fa79ca380d8971f41
PunChen <chainhow@outlook.com>
2025-05-25 12:44:04 +0800

    add color dye item translations

--- src/main/java/com/punchen/morecolorblocks/generator/MCBSENProvider.java
+++ src/main/java/com/punchen/morecolorblocks/generator/MCBSENProvider.java
@@ -2,6 +2,7 @@ package com.punchen.morecolorblocks.generator;
 
 import com.punchen.morecolorblocks.block.ColorBlocks;
 import com.punchen.morecolorblocks.colors.ConfigColor;
+import com.punchen.morecolorblocks.items.ColorItems;
 import com.punchen.morecolorblocks.utils.Reference;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
@@ -24,6 +25,9 @@ public class MCBSENProvider extends FabricLanguageProvider {
         }
         for (ConfigColor color : ColorBlocks.colorBlockPowderMap.keySet()) {
             translationBuilder.add(ColorBlocks.colorBlockPowderMap.get(color), color.name().toLowerCase() + "_powder");
+        }
+        for (ConfigColor color : ColorItems.colorDyeItemMap.keySet()) {
+            translationBuilder.add(ColorItems.colorDyeItemMap.get(color), color.name().toLowerCase() + "_dye");
         }
         translationBuilder.add(Reference.MOD_NAME, Reference.MOD_NAME);
 //        translationBuilder.add(ColorBlocks.BASE_COLOR_BLOCK, BASE_COLOR_BLOCK_NAME_EN);
